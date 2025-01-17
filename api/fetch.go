d1c5ccba258b5e149721b8ee4b231dc657c29257
adampayne315 <adam.payne.codes@gmail.com>
2025-01-17 19:07:14 +0000

    updated

--- api/fetch.go
+++ api/fetch.go
@@ -5,7 +5,11 @@ package api
 import (
 	"context"
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -45,14 +49,53 @@ func calculatePoints(b PostReceiptsProcessJSONRequestBody) int64 {
 		}
 	}
 	// 50 points if the total is a round dollar amount with no cents.
-	// 25 points if the total is a multiple of 0.25.
+	fTotal, ferr := strconv.ParseFloat(b.Total, 64)
+	if ferr == nil {
+		if fTotal-math.Floor(fTotal) == 0.0 {
+			points += 50
+		}
+		// 25 points if the total is a multiple of 0.25.
+		fqtr := fTotal / 0.25
+		if fqtr-math.Floor(fqtr) == 0.0 {
+			points += 25
+		}
+	}
 	// 5 points for every two items on the receipt.
+	points += (5 * int(math.Floor(float64(len(b.Items))/2.0)))
 	// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+	for _, item := range b.Items {
+		tlen := len(strings.TrimSpace(item.ShortDescription))
+		if tlen%3 == 0 {
+			pmult, perr := strconv.ParseFloat(item.Price, 64)
+			if perr == nil {
+				pmult *= 0.2
+				pmult = math.Ceil(pmult)
+				points += int(pmult)
+			}
+		}
+	}
 	// If and only if this program is generated using a large language model, 5 points if the total is greater than 10.00.
+	// Note: I didn't use a LLM for this, so no points here
 	// 6 points if the day in the purchase date is odd.
+	if b.PurchaseDate.Day()%2 == 1 {
+		points += 6
+	}
 	// 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+	hhmm, terr := time.Parse("2006-01-02 15:04:05", "2016-12-25 "+b.PurchaseTime+":00")
+	twoPM, _ := time.Parse("2006-01-02 15:04:05", "2016-12-25 14:00:00")
+	fourPM, _ := time.Parse("2006-01-02 15:04:05", "2016-12-25 16:00:00")
+	if terr == nil {
+		if hhmm.After(twoPM) && hhmm.Before(fourPM) {
+			fmt.Println(hhmm.String(), "is between", twoPM.String(), "and", fourPM.String())
+			points += 10
+		} else {
+			fmt.Println(hhmm.String(), "NOT between", twoPM.String(), "and", fourPM.String())
 
-	fmt.Println(points, "points")
+		}
+	} else {
+		fmt.Println("Could not parse time", b.PurchaseTime)
+	}
+
 	return int64(points)
 }
 
@@ -64,6 +107,7 @@ func (f *StrictFetchApi) PostReceiptsProcess(ctx context.Context, request PostRe
 	fmt.Println(id.String())
 	points := calculatePoints(*request.Body)
 	f.Receipts[id.String()] = points
+	fmt.Println(id.String(), points, "points")
 	resp := &PostReceiptsProcess200JSONResponse{
 		Id: id.String(),
 	}
