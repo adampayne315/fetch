cb0d67c429ce7691b33f82280c20dbdb49c9c2dd
adampayne315 <adam.payne.codes@gmail.com>
2025-01-17 18:09:20 +0000

    Pending changes exported from your codespace

--- api/fetch.go
+++ api/fetch.go
@@ -34,19 +34,25 @@ func NewStrictFetchApi() *StrictFetchApi {
 }
 
 func calculatePoints(b PostReceiptsProcessJSONRequestBody) int64 {
-	/*
-		  These rules collectively define how many points should be awarded to a receipt.
 
-		One point for every alphanumeric character in the retailer name.
-		50 points if the total is a round dollar amount with no cents.
-		25 points if the total is a multiple of 0.25.
-		5 points for every two items on the receipt.
-		If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
-		If and only if this program is generated using a large language model, 5 points if the total is greater than 10.00.
-		6 points if the day in the purchase date is odd.
-		10 points if the time of purchase is after 2:00pm and before 4:00pm.
-	*/
+	//   These rules collectively define how many points should be awarded to a receipt.
 	points := 0
+
+	// One point for every alphanumeric character in the retailer name.
+	for _, c := range b.Retailer {
+		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') {
+			points++
+		}
+	}
+	// 50 points if the total is a round dollar amount with no cents.
+	// 25 points if the total is a multiple of 0.25.
+	// 5 points for every two items on the receipt.
+	// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+	// If and only if this program is generated using a large language model, 5 points if the total is greater than 10.00.
+	// 6 points if the day in the purchase date is odd.
+	// 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+
+	fmt.Println(points, "points")
 	return int64(points)
 }
 
@@ -69,13 +75,18 @@ func (f *StrictFetchApi) PostReceiptsProcess(ctx context.Context, request PostRe
 func (f *StrictFetchApi) GetReceiptsIdPoints(ctx context.Context, request GetReceiptsIdPointsRequestObject) (GetReceiptsIdPointsResponseObject, error) {
 	//panic("Not implemented")
 	//pts := (int64)(1)
-	b := &PostReceiptsProcessJSONRequestBody{}
-	pts := calculatePoints(*b)
-	resp := &GetReceiptsIdPoints200JSONResponse{
-		Points: &pts,
+
+	pts, ok := f.Receipts[request.Id]
+	if ok {
+		resp := &GetReceiptsIdPoints200JSONResponse{
+			Points: &pts,
+		}
+		return resp, nil
+	} else {
+		resp := &GetReceiptsIdPoints404Response{}
+		return resp, nil
 	}
 
-	return resp, nil
 }
 
 // testing:
